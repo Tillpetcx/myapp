b62d0a65fcf5eac166d32bf0f2bdfd55efcab904
Tillpet <1322532661@qq.com>
2026-02-07 18:53:31 +0800

    单一主题配置

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  darkMode: "class",
+  plugins: [],
+};
+
+export default config;
